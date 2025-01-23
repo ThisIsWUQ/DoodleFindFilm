b0b0476f2029cd9b3b0090fdb0b411180205a451
ThisIsWUQ <113877939+ThisIsWUQ@users.noreply.github.com>
2025-01-23 11:02:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-#__import__('pysqlite3')
-#import sys
-#sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 import numpy as np
 import  requests
-import sqlite3
+#import sqlite3
 import chromadb
 from chromadb.utils import embedding_functions
 from TMDB import api_response
