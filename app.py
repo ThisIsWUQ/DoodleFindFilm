1345b90a85a791cb45f94393f56259462a214b63
ThisIsWUQ <113877939+ThisIsWUQ@users.noreply.github.com>
2025-01-28 16:01:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from transformers import pipeline
 # Initialize ChromaDB client and collection
 # reference: https://github.com/TharinduMadhusanka/semantic-movie-search/blob/main/app.py
 # ---------------------------------------------------------------------------------
-chroma_client = chromadb.PersistentClient(path="tmdbtopmovies")
+chroma_client = chromadb.PersistentClient(path="tmdbtopmovies/6419c645-98d4-4fac-83b8-d06feffa5acd")
 sentence_transformer_ef = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L12-v2")
 collection = chroma_client.get_or_create_collection(name="movies_collection", embedding_function=sentence_transformer_ef)
 # ---------------------------------------------------------------------------------
