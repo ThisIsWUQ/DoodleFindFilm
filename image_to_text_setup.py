c05b42f43cb7cbf11bef718c8d422a0319f026df
ThisIsWUQ <113877939+ThisIsWUQ@users.noreply.github.com>
2025-02-07 15:41:13 +0100

    Update image_to_text_setup.py

--- image_to_text_setup.py
+++ image_to_text_setup.py
@@ -1,6 +1,9 @@
+# Set up and test an image-to-text model from  HuggingFace
 # reference: https://huggingface.co/tasks/image-to-text
+# ---------------------------------------------------------------------------------
 from transformers import pipeline
 
 # Load pretrained model
 pipe = pipeline("image-to-text", model="Salesforce/blip-image-captioning-large")
 print("model loaded successfully")
+# ---------------------------------------------------------------------------------
