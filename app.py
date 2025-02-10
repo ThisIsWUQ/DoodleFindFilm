acb815a7c61b3b983373ea80dd048bc46b73eec3
ThisIsWUQ <113877939+ThisIsWUQ@users.noreply.github.com>
2025-02-10 20:39:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
 if drawing_mode == 'point':
     point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
 stroke_color = st.sidebar.color_picker("Stroke color hex: ")
-bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
+#bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
 bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
 
 realtime_update = st.sidebar.checkbox("Update in realtime", True)
